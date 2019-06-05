532943be744e9e694186d58c39fdbc427d634904
sky-5462 <yjfengjunxuan@126.com>
2019-06-05 21:47:19 +0800

    finish lab6 part1

--- labcodes/lab6/kern/trap/trap.c
+++ labcodes/lab6/kern/trap/trap.c
@@ -230,7 +230,7 @@ trap_dispatch(struct trapframe *tf) {
         /* you should upate you lab1 code (just add ONE or TWO lines of code):
          *    Every TICK_NUM cycle, you should set current process's current->need_resched = 1
          */
-        /* LAB6 YOUR CODE */
+        /* LAB6 17343025 */
         /* you should upate you lab5 code
          * IMPORTANT FUNCTIONS:
 	     * sched_class_proc_tick
@@ -238,7 +238,8 @@ trap_dispatch(struct trapframe *tf) {
         ticks++;
         if (ticks == TICK_NUM) {
             ticks = 0;
-            current->need_resched = 1;
+            sched_class_proc_tick(current);
+            // current->need_resched = 1;
         }
         break;
     case IRQ_OFFSET + IRQ_COM1:
